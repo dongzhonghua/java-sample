d9a3e218f5a7f49ef2b9a29aeccc6f0f74471b4e
dongzhonghua <dongzhonghua03@kuaishou.com>
2021-02-18 11:39:28 +0800

    map

---
+++ src/main/java/xyz/dsvshx/map/HashMapClient.java
@@ -0,0 +1,14 @@
+package xyz.dsvshx.map;
+
+import java.util.HashMap;
+
+/**
+ * @author dongzhonghua
+ * Created on 2021-02-18
+ */
+public class HashMapClient {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a","a");
+    }
+}
